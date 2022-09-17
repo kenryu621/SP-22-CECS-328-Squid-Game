bdaddb7a5cc82dd149cd534db79e04fd9f3889fc
Kenry Yu <kenryu422@outlook.com>
2022-09-17 01:11:07 -0700

    New: Repository created

---
+++ Squid_Game/Squid_Game/studentsolver.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int solve(int x, int n) {
+	if (x <= 0 || n <= 0 || x > n) return 0;
+	else if (x == n) return 0;
+	else {
+		int[][][x] going;
+	}
+	return 0
+}
